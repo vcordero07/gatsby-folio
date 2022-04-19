2112477261fd48e8708dad7d2ad6ccddffa9ac2c
vcordero07 <vcordero07@gmail.com>
2022-04-19 16:19:28 -0400

    our hero is ready

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,48 @@
-import React from "react"
-import Image from "gatsby-image"
-import { Link } from "gatsby"
-import { graphql, useStaticQuery } from "gatsby"
-import SocialLinks from "../constants/socialLinks"
-// ...GatsbyImageSharpFluid
+import React from "react";
+import Image from "gatsby-image";
+import { Link } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
+import SocialLinks from "../constants/socialLinks";
+
+const query = graphql`
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
 
 const Hero = () => {
-  return <h2>hero component</h2>
-}
+  // const data = useStaticQuery(query);
+  // console.log(data);
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query);
 
-export default Hero
+  return (
+    <header className="hero">
+      <div className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+            <h1>I'm Aristotle</h1>
+            <h4>freelance web and mobile UI/UX Designer</h4>
+            <Link to="/contact" className="btn">
+              contact me
+            </Link>
+            <SocialLinks />
+          </div>
+        </article>
+        <Image fluid={fluid} className="hero-img" />
+      </div>
+    </header>
+  );
+};
+
+export default Hero;
