3369bbd8f046bf39e72fbed535e52e660c262782
vcordero07 <vcordero07@gmail.com>
2022-04-20 10:34:07 -0400

    cat services set

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,8 +1,26 @@
-import React from "react"
-import Title from "./Title"
-import services from "../constants/services"
+import React from "react";
+import Title from "./Title";
+import services from "../constants/services";
 const Services = () => {
-  return <h2>services component</h2>
-}
+  return (
+    <section className="section bg-grey">
+      <Title title="services" />
+      <div className="section-center services-center">
+        {services.map((service) => {
+          // console.log(service);
+          const { id, icon, title, text } = service;
+          return (
+            <article className="service" key={id}>
+              {icon}
+              <h4>{title}</h4>
+              <div className="underline"></div>
+              <p>{text}</p>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default Services
+export default Services;
--- src/constants/services.js
+++ src/constants/services.js
@@ -1,22 +1,22 @@
-import React from "react"
-import { FaCode, FaSketch, FaAndroid } from "react-icons/fa"
+import React from "react";
+import { FaCode, FaSketch, FaAndroid } from "react-icons/fa";
 export default [
   {
     id: 1,
     icon: <FaCode className="service-icon" />,
     title: "web development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `I'm cat swag kale chips chartreuse cliche lyft direct trade, cred post-ironic adaptogen affogato. Roof party vape taxidermy, waistcoat irony drinking vinegar lyft. `,
   },
   {
     id: 2,
     icon: <FaSketch className="service-icon" />,
     title: "web design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `I'm cat DIY 90's trust fund unicorn, occupy pok pok pinterest. Narwhal freegan sustainable tbh cray. Sriracha mlkshk pickled ethical, +1 distillery narwhal.`,
   },
   {
     id: 3,
     icon: <FaAndroid className="service-icon" />,
     title: "app design",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `I'm cat Kickstarter ugh drinking vinegar, truffaut art party ramps organic authentic trust fund chartreuse kinfolk. Yr copper mug mlkshk brooklyn pickled chambray actually organic slow-carb.`,
   },
-]
+];
